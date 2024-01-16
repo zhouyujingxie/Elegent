92d225b17c27312881d6d004dc1b9dae00d768c8
zhouyujingxie <2415482899@qq.com>
2024-01-16 08:40:24 +0800

    first commit

---
+++ Test.java
@@ -0,0 +1,20 @@
+/**
+ * 变量的定义过程
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        //1.声明变量
+        int money;
+        double height;
+        String name;
+        //2.赋值
+        money=100;
+        height=178.5;
+        name="张三";
+        //3.使用变量
+        System.out.println(money);
+        System.out.println(height);
+        System.out.println(name);
+    }
+}
